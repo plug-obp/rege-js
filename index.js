593f751944c9962826f1938766b2147541e8a1e4
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2024-05-22 18:08:13 +0200

    exports

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 import { readExpression } from "./src/RegeReader.js";
 import { RegeDependentSemantics, isAccepting } from "./src/RegeSemantics.js";
+
+export {readExpression, RegeDependentSemantics, isAccepting};
